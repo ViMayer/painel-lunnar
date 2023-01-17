906f5f726a68bca415fd3f60f9d0383640a45d0b
Victor Mayer <victorhmayer@yahoo.com.br>
2023-01-17 09:46:17 -0300

    Update isallowed.js

--- src/pages/api/users/isallowed.js
+++ src/pages/api/users/isallowed.js
@@ -1,15 +1,21 @@
 import { getAllowed, getAdmin } from './getallowed'
 
 export default async function isAllowed(req, res) {
-     const teste2 = process.env.TESTE_KEY2
+  const teste2 = process.env.TESTE_KEY2
   console.log(teste2)
-  console.log("teste2")
-  const teste = process.env.SHEET_KEY
+  console.log('teste2')
+  const SHEET_KEY = process.env.SHEET_KEY
+  console.log(SHEET_KEY)
+  console.log('SHEET_KEY')
+  const SHEET_KEY_STRING_PROCUTION = process.env.SHEET_KEY_STRING_PROCUTION
+  console.log(SHEET_KEY_STRING_PROCUTION)
+  console.log('SHEET_KEY_STRING_PROCUTION')
+  const SHEET_KEY_STRING_PREVIEW = process.env.SHEET_KEY_STRING_PREVIEW
+  console.log(SHEET_KEY_STRING_PREVIEW)
+  console.log('SHEET_KEY_STRING_PREVIEW')
+  const teste = process.env.TESTE_KEY
   console.log(teste)
-  console.log("SHEET_KEY")
-    const teste = process.env.TESTE_KEY
-  console.log(teste)
-  console.log("teste")
+  console.log('teste')
 
   try {
     const response = await getAllowed(req.body.email, res)
