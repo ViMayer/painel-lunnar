4b089fea0fcd4a14df160e0f32ce05cdd49667d7
Victor Mayer <victorhmayer@yahoo.com.br>
2023-01-17 09:30:35 -0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,9 @@ import { useSession } from "next-auth/react";
 import styles from "../styles/home.module.scss";
 
 export default function Home(req, res) {
+  async function Testar() {
+    alert("a");
+  }
   const { status } = useSession();
   if (status == "loading") {
     return <h1>CARREGANDO...</h1>;
@@ -11,6 +14,13 @@ export default function Home(req, res) {
   if (status == "authenticated") {
     return (
       <div className={styles.container}>
+        <button
+          onClick={() => {
+            Testar();
+          }}
+        >
+          TESTAR
+        </button>
         <div className={styles.holder}>
           <a href={`${defaultUrl + "/users"}`}>
             <h3>USUÁRIOS</h3>
