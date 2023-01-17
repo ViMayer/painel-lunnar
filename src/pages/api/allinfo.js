39f2b6ee53302cf3e4fb02d1d46985f7362eccb3
Victor Mayer <victorhmayer@yahoo.com.br>
2023-01-17 10:06:46 -0300

    Delete src/pages directory

--- src/pages/api/allinfo.js
+++
@@ -1,6 +0,0 @@
-import { getInfo } from './connection'
-
-export default async function infos(req, res) {
-  const response = await getInfo(req, res)
-  res.json(response)
-}
--- src/pages/api/users/read.js
+++
@@ -1,6 +0,0 @@
-import { getAllPermitted } from "./allpermitted";
-
-export default async function infos(req, res) {
-  const response = await getAllPermitted(req, res);
-  res.json(response);
-}
