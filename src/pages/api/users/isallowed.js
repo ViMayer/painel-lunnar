553623765001b0f53a1224f39ec3282f57c326a6
Victor Mayer <victorhmayer@yahoo.com.br>
2023-01-17 09:40:09 -0300

    Update isallowed.js

--- src/pages/api/users/isallowed.js
+++ src/pages/api/users/isallowed.js
@@ -4,6 +4,9 @@ export default async function isAllowed(req, res) {
   const teste = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL
   console.log(teste)
   console.log("teste")
+    const teste2 = process.env.TESTE_KEY
+  console.log(teste2)
+  console.log("teste2")
   try {
     const response = await getAllowed(req.body.email, res)
     // console.log('respostaALLOWED')
